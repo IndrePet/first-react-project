2c31beab5ea7869f27d032782dea6dea384ab677
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-05-23 17:37:06 +0300

    conditionals

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,40 @@
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = props => {
+  const [formStatus, setFormStatus] = useState(false);
+
   const saveExpenseDataHandler = enteredExpenseData => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.trunc(Math.random() * 1000).toString(),
     };
-
     props.onAddExpense(expenseData);
+    setFormStatus(false);
   };
 
+  const editingHandler = () => {
+    setFormStatus(true);
+  };
+  const cancelationHandler = () => {
+    setFormStatus(false);
+  };
+
+  if (formStatus) {
+    return (
+      <div className="new-expense">
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelationHandler}
+        />
+      </div>
+    );
+  }
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <button onClick={editingHandler}>Add New Expense</button>
     </div>
   );
 };
